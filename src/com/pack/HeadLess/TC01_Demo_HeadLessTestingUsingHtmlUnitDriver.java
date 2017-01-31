2e1ca5bb215e91223ade53d2ed0ff59000ceac2c
Nirimakhi-123 <nirimakhi@gmail.com>
2017-01-31 16:53:42 +0530

    updated.

--- src/com/pack/HeadLess/TC01_Demo_HeadLessTestingUsingHtmlUnitDriver.java
+++ src/com/pack/HeadLess/TC01_Demo_HeadLessTestingUsingHtmlUnitDriver.java
@@ -1,12 +1,18 @@
 package com.pack.HeadLess;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -68,9 +74,32 @@ public void verifyFaceBookTitleNew(){
 	Assert.assertTrue(FacebookTitle.contains("Selenium"));
 	
 	System.out.println("verifyFaceBookTitleNew().....Force fully Fail this test case");
-	
-}
-
-
 
 }
+
+@AfterMethod
+public void tearDown(ITestResult result)
+{
+
+// Here will compare if test is failing then only it will enter into if condition
+if(ITestResult.FAILURE==result.getStatus())
+{
+try 
+{
+// Create refernce of TakesScreenshot
+TakesScreenshot ts=(TakesScreenshot)driver;
+// Call method to capture screenshot
+File source=ts.getScreenshotAs(OutputType.FILE);
+
+// Copy files to specific location here it will save all screenshot in our project home directory and
+// result.getName() will return name of test case so that screenshot name will be same
+FileUtils.copyFile(source, new File("./Screenshots/"+result.getName()+".png"));
+
+System.out.println("Screenshot taken");
+} catch (Exception e)
+
+{System.out.println("Exception while taking screenshot "+e.getMessage());} 
+
+} //if
+} // method
+} //class end
